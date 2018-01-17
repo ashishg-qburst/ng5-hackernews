c0ba528e0bb1dcf409a605d3d30117b7c32aed54
ash <ashishg@qburst.com>
2018-01-17 18:04:57 +0530

    Fetch stories with children

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/forkJoin';
+
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/observable/forkJoin';
+import 'rxjs/add/observable/combineLatest';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -26,7 +30,16 @@ export class DataService {
     return Observable.forkJoin(...items);
   }
 
+  getItemWithDescendants(id) {
+    return this.getItem(id).mergeMap(item => {
+      return this.getItems(item.kids).map(kids => {
+        item.kids = kids;
+        return item;
+      });
+    });
+  }
+
   getItem(id) {
-    return this._http.get(`${HN_BASE_URL}/item/${id}.json`);
+    return this._http.get<any>(`${HN_BASE_URL}/item/${id}.json`);
   }
 }
