b10bf7e01c01fc15efc86cbbce821123f8f93b3d
ash <ashishg@qburst.com>
2018-01-18 13:19:58 +0530

    Fix threaded comment navigation and progressive loading

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -5,18 +5,49 @@ import { DataService } from '../data.service';
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.scss'],
-  host: { '(click)': 'getChildComments()' }
+  host: { '(click)': 'onClick($event)' }
 })
 export class CommentComponent implements OnInit {
 
   @Input() comment;
+  hasKids: boolean = false;
+  fetchedKids: boolean = false;
+  closedComment: boolean = true;
+
+  currentClasses: {};
+  setCurrentClasses() {
+    this.currentClasses =  {
+      'has-kids': this.hasKids,
+      'closed': this.closedComment
+    };
+  }
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.hasKids = (this.comment.kids && this.comment.kids.length > 0) ? true : false;
+    this.setCurrentClasses();
+  }
+
+  onClick(event) {
+    if(this.hasKids) {
+      if(this.closedComment) {
+        if(!this.fetchedKids) {
+          this.getChildComments();
+        }
+        this.closedComment = false;
+      } else {
+        this.closedComment = true;
+      }
+    }
+    this.setCurrentClasses();
+    event.stopPropagation();
   }
 
   getChildComments() {
-    this.dataService.getItems(this.comment.kids).subscribe(items => this.comment.children = items);
+    this.dataService.getItems(this.comment.kids).subscribe(items => {
+      this.comment.children = items;
+      this.fetchedKids = true;
+    });
   }
 }
