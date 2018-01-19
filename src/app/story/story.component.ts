d666ee51bb52c31163f395ebf9d082a4bb097053
ash <ashishg@qburst.com>
2018-01-19 11:25:14 +0530

    Update nav button styles

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -11,8 +11,7 @@ export class StoryComponent implements OnInit {
 
   story;
 
-  constructor(private dataService: DataService,
-              private route: ActivatedRoute) { }
+  constructor(private dataService: DataService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.getStory();
